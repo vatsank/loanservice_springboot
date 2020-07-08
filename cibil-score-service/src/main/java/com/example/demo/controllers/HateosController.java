c4548b22fe91af51bced2061752898d838e0f8dd
Srivatsan <'vatsank@gmail.com'>
2020-07-08 10:54:40 +0530

    Hateos Example Added

---
+++ cibil-score-service/src/main/java/com/example/demo/controllers/HateosController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.CibilScore;
+import com.example.demo.services.CibilScoreService;
+
+@RestController
+public class HateosController {
+
+	@Autowired
+	private CibilScoreService service;
+	
+	@GetMapping(path = "/api/v2/cibilscores")
+	public List<CibilScore> findAll(){
+		
+		return this.service.findAll();
+	}
+	
+	@GetMapping(path = "/api/v2/cibilscores/{id}")
+	public List<CibilScore> findById(@PathVariable("id") int id){
+		
+		return this.service.findAll();
+	}
+	
+}
