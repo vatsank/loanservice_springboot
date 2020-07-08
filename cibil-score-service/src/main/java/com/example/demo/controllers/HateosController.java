b13c09e43057c9fc3310d80689132a676a68dc05
Srivatsan <'vatsank@gmail.com'>
2020-07-08 11:30:20 +0530

    Update

--- cibil-score-service/src/main/java/com/example/demo/controllers/HateosController.java
+++ cibil-score-service/src/main/java/com/example/demo/controllers/HateosController.java
@@ -3,6 +3,10 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +21,35 @@ public class HateosController {
 	private CibilScoreService service;
 	
 	@GetMapping(path = "/api/v2/cibilscores")
-	public List<CibilScore> findAll(){
+	public CollectionModel<CibilScore> findAll(){
 		
 		List<CibilScore> scores= this.service.findAll();
 		
+		for(CibilScore eachScore:scores) {
+			
+			Link link = WebMvcLinkBuilder.linkTo(HateosController.class)
+					             .slash("api/v2/cibilscores/"+eachScore.getId())
+					                 .withSelfRel();
+			
+			eachScore.add(link);
+		}
+		
+		Link link = WebMvcLinkBuilder.linkTo(HateosController.class).withSelfRel();
+		CollectionModel<CibilScore> model = CollectionModel.of(scores, link);
+		
+		return model;
 	}
 	
 	@GetMapping(path = "/api/v2/cibilscores/{id}")
-	public CibilScore findById(@PathVariable("id") int id){
+	public EntityModel<CibilScore> findById(@PathVariable("id") int id){
 		
-		return this.service.findById(id);
+		CibilScore score = this.service.findById(id);
+		
+		Link link = WebMvcLinkBuilder.linkTo(HateosController.class).withSelfRel();
+
+		EntityModel<CibilScore> model = EntityModel.of(score,link);
+		
+		return model;
 	}
 	
 }
