6331e6ad9aa71e3646c9b4957bb4eb5dd9f44397
Srivatsan <'vatsank@gmail.com'>
2020-07-09 09:37:11 +0530

    Update

--- cibilscore-client-service/src/main/java/com/example/demo/controllers/ClientController.java
+++ cibilscore-client-service/src/main/java/com/example/demo/controllers/ClientController.java
@@ -1,5 +1,19 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
 public class ClientController {
 
+	
+	@Autowired
+	private RestTemplate template;
+	
+	@GetMapping(path = "/api/v1/points")
+	public String fetchScores() {
+		
+		return template.getForObject("http://CIBILE-SCORE-SERVICE/api/v1/cibilscores",String.class);
+		
+	}
 }
